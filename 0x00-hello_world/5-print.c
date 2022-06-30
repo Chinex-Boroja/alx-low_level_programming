d578cbe8a609d4e2baf309ceddfbe3396ac22fe5
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-06-30 10:46:03 -0700

    Print a description

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints the phrase, proper grammar,
+ * but the out is a piece of art
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
