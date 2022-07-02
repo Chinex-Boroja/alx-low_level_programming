b0630423ce8cad2b4f778822964c308110c3367a
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-07-01 18:28:03 -0700

    Print number followed by newline

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbes of base 10 from 0,
+ * followed by a new line, using putchar keyword
+ * Return: 0 which means success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n)
+	}
+	putchar('\n');
+	return (0);
+}
