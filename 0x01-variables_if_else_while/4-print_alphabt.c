eb4987b5c8be73f786d6f482d4b40e6349b4364d
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-07-01 18:03:30 -0700

    Lower case C

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-	char ch = a;
+	char ch = 'a';
 
-	while (ch <= z)
+	while (ch <= 'z')
 	{
-		if (ch != e && ch != q)
+		if (ch != 'e' && ch != 'q')
 		{
 			putchar(ch);
 		}
 		ch++;
 	}
-	putchar(n);
+	putchar('\n');
 	return (0);
 }
