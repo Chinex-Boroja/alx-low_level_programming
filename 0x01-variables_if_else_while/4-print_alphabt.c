f931ad7dce50dd8e8cc680e23fcfda97e782f967
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-07-01 17:57:25 -0700

    Print in lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,22 +2,20 @@
 
 /**
  * main - prints the alphabet in lowercase,
- * Return: Always 0 
+ * Return: Always 0
  */
-
 int main(void)
 {
-
 	char ch = a;
 
 	while (ch <= z)
-	{	
+	{
 		if (ch != e && ch != q)
 		{
 			putchar(ch);
 		}
 		ch++;
-	}		
+	}
 	putchar(n);
 	return (0);
 }
