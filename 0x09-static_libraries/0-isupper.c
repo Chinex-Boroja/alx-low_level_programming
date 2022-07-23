8ba7751d1bcfe3357df8f54610142e850c3cfadd
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-07-23 00:02:25 -0700

    Static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - function that verifies if a characteris uppercase or not
+ * @c: tested character
+ * Return: return 1 if it is uppercase, 0 if not
+ */
+
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
