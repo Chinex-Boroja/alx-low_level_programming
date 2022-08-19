a729cd8e64db69137f05b705fb2c8d49dfe831bb
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-08-18 21:47:56 -0700

    Print rep of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary equivalent of a decimal number
- * @n: number to print in binary
- *
- * Return: 0
+ * print_binary - prints the binary representation of a number
+ * @n: parameter
  */
 void print_binary(unsigned long int n)
 {
